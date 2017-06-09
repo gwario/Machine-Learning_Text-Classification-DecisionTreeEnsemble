2f9084663a22708e12d8f5e8fa059a31462f1751
Mario Gastegger <mario.gastegger@gmail.com>
2017-06-09 14:41:13 +0200

    python3 compatibility

--- preprocessor.py
+++ preprocessor.py
@@ -17,69 +17,73 @@ import logging as log
 
 import sys
 
-reload(sys)  
-sys.setdefaultencoding('utf8')
+if sys.version_info >= (3, 0):
+    import importlib
 
+    importlib.reload(sys)
+else:
+    reload(sys)
+    sys.setdefaultencoding('utf8')
 
 # Fix Python 2.x.
 try:
     UNICODE_EXISTS = bool(type(unicode))
 except NameError:
-    unicode = lambda s: str(s)
+    unicode = lambda s, errors: str(s)
+
 
 # Source: http://bbengfort.github.io/tutorials/2016/05/19/text-classification-nltk-sckit-learn.html
 class NLTKPreprocessor(BaseEstimator, TransformerMixin):
+    def __init__(self, stopwords=None, punct=None,
+                 lower=True, strip=True):
+        self.lower = lower
+        self.strip = strip
+        self.stopwords = stopwords or set(sw.words('english'))
+        self.punct = punct or set(string.punctuation)
+        self.lemmatizer = WordNetLemmatizer()
 
-	def __init__(self, stopwords=None, punct=None,
-	             lower=True, strip=True):
-	    self.lower      = lower
-	    self.strip      = strip
-	    self.stopwords  = stopwords or set(sw.words('english'))
-	    self.punct      = punct or set(string.punctuation)
-	    self.lemmatizer = WordNetLemmatizer()
+    def fit(self, X, y=None):
+        return self
 
-	def fit(self, X, y=None):
-	    return self
+    def inverse_transform(self, X):
+        return [" ".join(doc) for doc in X]
 
-	def inverse_transform(self, X):
-	    return [" ".join(doc) for doc in X]
+    def transform(self, X):
+        log.info("Tokenizing documents (this may take a while...)")
+        return [
+            list(self.tokenize(doc)) for doc in X
+        ]
 
-	def transform(self, X):
-		log.info("Tokenizing documents (this may take a while...)")
-		return [
-			list(self.tokenize(doc)) for doc in X
-		]
+    def tokenize(self, document):
+        # Break the document into sentences
+        document = unicode(document, errors='replace')
+        for sent in sent_tokenize(document):
+            # Break the sentence into part of speech tagged tokens
+            for token, tag in pos_tag(wordpunct_tokenize(sent)):
+                # Apply preprocessing to the token
+                token = token.lower() if self.lower else token
+                token = token.strip() if self.strip else token
+                token = token.strip('_') if self.strip else token
+                token = token.strip('*') if self.strip else token
 
-	def tokenize(self, document):
-	    # Break the document into sentences	    
-	    document = unicode(document, errors='replace')
-	    for sent in sent_tokenize(document):
-	        # Break the sentence into part of speech tagged tokens
-	        for token, tag in pos_tag(wordpunct_tokenize(sent)):
-	            # Apply preprocessing to the token
-	            token = token.lower() if self.lower else token
-	            token = token.strip() if self.strip else token	           	           
-	            token = token.strip('_') if self.strip else token
-	            token = token.strip('*') if self.strip else token	        
+                # If stopword, ignore token and continue
+                if token in self.stopwords:
+                    continue
 
-	            # If stopword, ignore token and continue
-	            if token in self.stopwords:
-	                continue
+                # If punctuation, ignore token and continue
+                if all(char in self.punct for char in token):
+                    continue
 
-	            # If punctuation, ignore token and continue
-	            if all(char in self.punct for char in token):
-	                continue
+                # Lemmatize the token and yield
+                lemma = self.lemmatize(token, tag)
+                yield lemma
 
-	            # Lemmatize the token and yield
-	            lemma = self.lemmatize(token, tag)
-	            yield lemma
+    def lemmatize(self, token, tag):
+        tag = {
+            'N': wn.NOUN,
+            'V': wn.VERB,
+            'R': wn.ADV,
+            'J': wn.ADJ
+        }.get(tag[0], wn.NOUN)
 
-	def lemmatize(self, token, tag):
-	    tag = {
-	        'N': wn.NOUN,
-	        'V': wn.VERB,
-	        'R': wn.ADV,
-	        'J': wn.ADJ
-	    }.get(tag[0], wn.NOUN)
-
-	    return self.lemmatizer.lemmatize(token, tag)
+        return self.lemmatizer.lemmatize(token, tag)
