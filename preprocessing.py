cfb7750ef3f3eaf125472a9731d619e3fee0951a
Alexander Heemann <alexanderheemann@gmail.com>
2017-06-16 12:02:55 +0200

    Adapt cl arguments and token separator

--- preprocessing.py
+++ preprocessing.py
@@ -9,7 +9,7 @@ def get_args(args_parser):
     """Parses and returns the command line arguments."""
 
     args_parser.add_argument('--data', metavar='FILE', type=argparse.FileType('r'), help='Data to be preprocessed.')
-    args_parser.add_argument('--filename', metavar='FILE', type=argparse.FileType('r'), help='The filename for the new processed data.')
+    args_parser.add_argument('--filename', metavar='FILE', type=argparse.FileType('w'), help='The filename for the new processed data.')
     return args_parser.parse_args()
 
 
@@ -25,12 +25,10 @@ def preprocess(dataset, key):
 	log.info("Preprocessing data")
 
 	tokens = nltk_preprocessor.transform(dataset[key])
-	joined = [' '.join(t) for t in tokens]
-	# print(joined)
-	# print(dataset)
+	joined = [';'.join(t) for t in tokens]
 	series = pd.Series(joined, index=dataset.index)
 	dataset['Tokens'] = series
-	# return np.insert(dataset, dataset.shape[1], joined, axis=1)
+	
 	return dataset
 
 if __name__ == '__main__':
@@ -44,4 +42,4 @@ if __name__ == '__main__':
 	new_data = preprocess(x, get_key(dataset_type))
 	new_data = pd.concat([new_data, y], axis=1)
 	io.save_data(new_data, args.filename)
-	log.info("Saved to file: {}".format(filename))
+	log.info("Saved to file: {}".format(args.filename))
