44df88900489dcd5c9d0c3034d55b32ec1c9135e
DesislavaVelinova <velinova.desislava@gmail.com>
2017-07-02 20:29:29 +0200

    Update report.py

--- report.py
+++ report.py
@@ -105,8 +105,34 @@ def print_fitting_report(pipeline, dt_fitting, x_train, y_train, min_estimators=
     else:
         log.debug("Fitted {} data points.".format(len(y_train)))
         log.debug("Fitting done in {}".format(dt_fitting))
+  
+def print_feature_importances_report(pipeline, dt_fitting, x_train, y_train, n_estimators=None, importances = None, indices = None):
 
+    if n_estimators:
+ 
+      # Print the feature ranking
+      log.debug("Print the feature ranking...")
+      for f in range(x_train.shape[1]):
+          print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
+      
+      # Plot the feature importances
+      plt.figure()
+      plt.title("Feature importances")
+      plt.bar(range(x_train.shape[1]), importances[indices], color="r", yerr=std[indices], align="center")
+      plt.xticks(range(x_train.shape[1]), indices)
+      plt.xlim([-1, x_train.shape[1]])
 
+      log.debug("Fitted {} data points.".format(len(y_train)))
+      log.debug("Fitting and feature importances done in {}".format(dt_fitting))
+
+      plt.savefig('feature_importances.png', transparent=True)
+      #plt.show()
+
+    else:
+        log.debug("Fitted {} data points.".format(len(y_train)))
+        log.debug("Fitting done in {}".format(dt_fitting))
+
+        
 def print_evaluation_report(pipeline, dt_evaluation, y_pred, y_true):
     """Prints the cross-validation report."""
 
